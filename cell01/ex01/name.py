e3ac01c5d4388cd87031f25cc7d17e1a7b9fa6a3
S0dixmm <143612070+S0dixmm@users.noreply.github.com>
2026-02-09 18:03:45 +0700

    Add files via upload

---
+++ cell01/ex01/name.py
@@ -0,0 +1,6 @@
+first_name = "Phichada"
+last_name = "kaewsiri"
+
+whole_name = ((first_name) + " " + (last_name))
+
+print(f"{whole_name}\n")
---
+++ cell01/ex02/age.py
@@ -0,0 +1,4 @@
+age = 20
+my_age = (age+42)
+
+print(f"{my_age}\n")
